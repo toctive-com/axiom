0ab907bff00f4d64e8f081afd60a8531c6a19ed9
Sameh A. Elalfi <sameh.elalfi.mail@gmail.com>
2023-06-29 23:04:35 +0300

    fix: move handle() to Maintenance in MakeX package

--- packages/makex-example/src/bootstrap/Maintenance.ts
+++ packages/makex-example/src/bootstrap/Maintenance.ts
@@ -1,16 +1,4 @@
-import { PathOrFileDescriptor, readFileSync } from "node:fs";
+import { Maintenance as MaintenanceClass } from "@toctive/makex";
 
-export class Maintenance {
-  // TODO add interface for maintenance configs
-  private static config: {};
-
-  public static handle(configFile: PathOrFileDescriptor) {
-    // FIXME if failed to parse json data, return 503 status code
-    Maintenance.config = JSON.parse(
-      readFileSync(configFile, { encoding: "utf-8" })
-    );
-
-    console.log(Maintenance.config);
-  }
-}
+export class Maintenance extends MaintenanceClass {}
 export default Maintenance;
