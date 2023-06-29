f3aa1d0ea1989cbec22d9201578bc347f73d36fd
Sameh A. Elalfi <sameh.elalfi.mail@gmail.com>
2023-06-29 23:06:12 +0300

    feat: create main() that handle the app & kernel

---
+++ packages/makex-example/src/index.ts
@@ -0,0 +1,34 @@
+import { runApp } from "@/bootstrap";
+import { HttpKernel } from "@toctive/makex";
+
+console.time("MakeX-FullCycle");
+
+/**
+ * Run The Application
+ * --------------------------------------------------------------------------
+ * Once we have the application, we can handle the incoming request using
+ * the application's HTTP kernel. Then, we will send the response back
+ * to this client's browser, allowing them to enjoy our application.
+ *
+ */
+async function main() {
+  // Load all developer files (e.g. routes, middleware, class ...)
+  const app = await runApp();
+
+  // Load framework kernel which includes HTTP server and will handle requests
+  const kernel: HttpKernel = app.make(HttpKernel);
+
+  // Wait for incoming request and return it with empty response object
+  const { request, response } = await kernel.captureRequest();
+
+  // Handle the request using the response object
+  const result = await kernel.handle(request, response);
+
+  // Send response to client
+  await result.send();
+
+  // Close the server and terminate all incoming requests
+  await kernel.terminate(request, response);
+}
+
+main().then(() => console.timeEnd("MakeX-FullCycle"));
