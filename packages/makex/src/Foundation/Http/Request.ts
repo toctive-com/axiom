e965c8e223e77a4d0bfa6f2c3289b32185bab149
Sameh A. Elalfi <sameh.elalfi.mail@gmail.com>
2023-07-02 00:30:10 +0300

    feat: Request.locals to store data across the app

--- packages/makex/src/Foundation/Http/Request.ts
+++ packages/makex/src/Foundation/Http/Request.ts
@@ -1,4 +1,11 @@
-import { IncomingMessage } from "http";
+import { IncomingMessage } from "node:http";
 
 export class HttpRequest extends IncomingMessage {
+  static locals: any = {};
+  public get locals(): any {
+    return HttpRequest.locals;
+  }
+  public set locals(value: any) {
+    HttpRequest.locals = { ...HttpRequest.locals, value };
+  }
 }
