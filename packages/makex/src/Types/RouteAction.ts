3e8b7dd530f042640dd58e1d9d398e34b6e23206
Sameh A. Elalfi <sameh.elalfi.mail@gmail.com>
2023-07-10 00:58:17 +0300

    feat: add route action types

---
+++ packages/makex/src/Types/RouteAction.ts
@@ -0,0 +1,15 @@
+import { HttpRequest, HttpResponse, Application } from "../Foundation";
+
+export type RouteActionParameters = {
+  next?: Function;
+  request?: HttpRequest;
+  req?: HttpRequest;
+  response?: HttpResponse;
+  res?: HttpResponse;
+  app?:Application;
+  [key: string]: unknown;
+};
+
+export type RouteAction = (
+  params: RouteActionParameters
+) => unknown | Promise<unknown>;
