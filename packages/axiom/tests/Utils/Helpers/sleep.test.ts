4ceaba142de848edb2365649f0ccff0994d19f94
Sameh A. Elalfi <sameh.elalfi.mail@gmail.com>
2023-07-12 22:22:00 +0300

    chore: add Vitest

---
+++ packages/axiom/tests/Utils/Helpers/sleep.test.ts
@@ -0,0 +1,9 @@
+import { it, expect } from "vitest";
+import { sleep } from "../../../src";
+
+it("should sleep for 100ms", async () => {
+  const startTime = Date.now();
+  await sleep(100);
+  const endTime = Date.now();
+  expect(endTime - startTime).toBeGreaterThanOrEqual(100);
+});
