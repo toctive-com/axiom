79c6b388e851cfea970ad341d9af87977091d4b9
Sameh A. Elalfi <sameh.elalfi.mail@gmail.com>
2023-07-12 18:49:15 +0300

    feat: add URL facade

---
+++ packages/axiom/src/Utils/Facades/Url.ts
@@ -0,0 +1,12 @@
+export class Url {
+  static trim(url: string) {
+    return url
+      .split("/")
+      .filter((i) => i !== "")
+      .join("/");
+  }
+
+  static join(...urls: string[]) {
+    return urls.map(Url.trim).join("/");
+  }
+}
