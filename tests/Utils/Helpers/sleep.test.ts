7231aa86d843db6b8e93e757d88b19e538fbe9ac
Sameh A. Elalfi <sameh.elalfi.mail@gmail.com>
2023-07-14 17:00:37 +0300

    test: add additional test for sleep util

--- tests/Utils/Helpers/sleep.test.ts
+++ tests/Utils/Helpers/sleep.test.ts
@@ -1,9 +1,16 @@
 import { it, expect } from 'vitest';
-import { sleep } from '../../../src';
+import { sleep } from '@/Utils';
 
 it('should sleep for 100ms', async () => {
   const startTime = Date.now();
   await sleep(100);
   const endTime = Date.now();
   expect(endTime - startTime).toBeGreaterThanOrEqual(100);
+});
+
+it('should sleep for 200ms', async () => {
+  const startTime = Date.now();
+  await sleep(200);
+  const endTime = Date.now();
+  expect(endTime - startTime).toBeGreaterThanOrEqual(200);
 });
