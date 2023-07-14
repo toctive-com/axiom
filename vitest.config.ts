bcf8226ed23411cfce59fd22573b9cdf513a54d6
Sameh A. Elalfi <sameh.elalfi.mail@gmail.com>
2023-07-14 16:49:08 +0300

    test: add tsconfigPaths support to vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,12 @@ import { defineConfig } from 'vitest/config';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  plugins: [tsconfigPaths({
+    ignoreConfigErrors: true,
+    parseNative: true,
+    loose: true,
+    root: './',
+  })],
 
   test: {
     coverage: {
