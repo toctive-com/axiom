7f19bd4b78c8a581182b8edfaa8637dd74a01fa3
Sameh A. Elalfi <sameh.elalfi.mail@gmail.com>
2023-07-22 16:08:21 +0300

    🔎 ref: add try catch to execute method

--- src/Core/Routing/RouteServiceProvider.ts
+++ src/Core/Routing/RouteServiceProvider.ts
@@ -3,24 +3,53 @@ import { ServiceProvider } from '../ServiceProvider';
 import { Router } from './Router';
 
 export class RouteServiceProvider extends ServiceProvider {
+  /**
+   * Registers routes on a router and handles executing the matched route and
+   * writing the result to the response.
+   *
+   * @param {Router} router - The `router` parameter is an instance of the
+   * `Router` class. It is used to define and match routes for incoming
+   * requests.
+   *
+   */
   protected registerRoutes(router: Router): void {
     this.app.add((req, res, next) => {
       const matchedRoute = router.match(req);
       let result = null;
 
       if (matchedRoute) {
-        result = matchedRoute.execute(req, res);
+        try {
+          result = matchedRoute.execute(req, res);
+        } catch (error) {
+          // TODO add error handler object
+          return res.write('Error: ' + error.message);
+        }
+
         // if result is string write it to response
         if (typeof result === 'string') {
           return res.write(result);
         } else if (typeof result === 'object') {
           return res.write(stringify(result));
+        } else if (typeof result === 'undefined') {
+          return res;
         } else {
           return res.write(result?.toString() ?? result);
         }
       }
 
       return next();
+    });
+  }
+
+  /**
+   * Adds a route to the app that writes "Route not found" to the response.
+   *
+   */
+  protected handlerNotFoundError(): void {
+    this.app.add((req, res) => {
+      // TODO add default views for errors
+      res.statusCode = 404;
+      res.write('Route not found');
     });
   }
 }
