e85f6a6d31a31e0da51620f593400fc72a6e20ad
Sameh A. Elalfi <sameh.elalfi.mail@gmail.com>
2023-08-12 17:37:51 +0300

    test: call a stack of matched routes

--- tests/core/routing/routes.test.ts
+++ tests/core/routing/routes.test.ts
@@ -1,5 +1,6 @@
 import { Request, Route, Router } from '@/core';
 import { makeFunctionsChain } from '@/utils/helpers/makeFunctionsChain';
+import exp from 'node:constants';
 import { METHODS } from 'node:http';
 import { Socket } from 'node:net';
 import { describe, expect, it, vi } from 'vitest';
@@ -112,7 +113,7 @@ describe('Make routes and test if they works as expected', () => {
     const routeAll = router.routes[0];
     const routeAny = router.routes[1];
 
-    const methods = METHODS.map(m => m.toLowerCase())
+    const methods = METHODS.map((m) => m.toLowerCase());
 
     expect(routeAll).toBeInstanceOf(Route);
     expect((routeAll as Route).httpMethods).toEqual(methods);
@@ -165,7 +166,7 @@ describe('Make routes and test if they works as expected', () => {
   it('should not stack overflow with a large sync stack of routes', () => {
     const router = new Router();
     const stackSize = 10000;
-    const testFunc = vi.fn()
+    const testFunc = vi.fn();
 
     router.get('/test', ({ req, next }) => {
       req.locals.counter = 0;
@@ -203,11 +204,7 @@ describe('Make routes and test if they works as expected', () => {
       res: null,
     });
 
-    (matchedRoutes[0] as Route).dispatch(
-      request,
-      null,
-      nextFunctionsStack,
-    );
+    (matchedRoutes[0] as Route).dispatch(request, null, nextFunctionsStack);
     expect(request.locals.counter).toBe(stackSize);
     expect(request.locals.called).toBeTruthy();
     expect(matchedRoutes).toBeTruthy();
@@ -244,5 +241,73 @@ describe('Make routes and test if they works as expected', () => {
     expect(request.locals.counter).toBe(6000);
     expect(request.locals.called).toBeTruthy();
     expect(matchedRoutes).toBeTruthy();
+  });
+
+  it('should call a stack of actions with the same request', () => {
+    const request = new Request(new Socket());
+    request.url = '/test-stack';
+    request.method = 'get';
+    const stack = [
+      ({ req, next }) => {
+        req.locals.counter = 0;
+        next();
+      },
+      ({ req, next }) => {
+        req.locals.counter++;
+        next();
+      },
+      ({ req, next }) => {
+        req.locals.counter++;
+        next();
+      },
+    ];
+
+    const router = new Router();
+    router.get('/test-stack', stack);
+
+    const matchedRoutes = router.match(request);
+    (matchedRoutes[0] as Route).dispatch(request, null);
+    expect(request.locals.counter).toBe(2);
+  });
+
+  it('should call a stack of routes from multiple routers with the same request', () => {
+    const request = new Request(new Socket());
+    request.url = '/test-stack';
+    request.method = 'get';
+
+    const testFunc = vi.fn();
+    const stack = [
+      ({ req, next }) => {
+        testFunc();
+        req.locals.counter = 0;
+        next();
+      },
+      ({ req, next }) => {
+        testFunc();
+        req.locals.counter++;
+        next();
+      },
+      ({ req, next }) => {
+        testFunc();
+        req.locals.counter++;
+        next();
+      },
+    ];
+
+    const routerA = new Router();
+    routerA.get('/test-stack', stack[0]);
+    const routerB = new Router();
+    routerB.get('/test-stack', stack[1]);
+    const routerC = new Router();
+    routerC.get('/test-stack', stack[2]);
+
+    const router = new Router();
+    router.use(routerA);
+    router.use(routerB);
+    router.use(routerC);
+    router.dispatch(request, null);
+
+    expect(testFunc).toBeCalledTimes(3);
+    expect(request.locals.counter).toBe(2);
   });
 });
